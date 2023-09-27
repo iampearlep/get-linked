cca3d5583e9b507793c986a8b5621cb3d4c2ccbb
iampearlep <damilolaprincess4@gmail.com>
2023-09-27 12:29:44 +0100

    update rules and attributes component

--- app/Attributes.jsx
+++ app/Attributes.jsx
@@ -1,50 +1,57 @@
 import Image from "next/image";
+import Ellipse from "../public/images/Ellipse 1.svg";
 import Judges from "../public/images/judges.svg";
 import Star from "../public/images/star pu.svg";
 import Star1 from "../public/images/star.svg";
+
 const Attributes = () => {
   return (
     <div className="bg-[#150E28] text-white border-b border-[#3d364d]">
-      <div className="w-10/12 flex flex-row mx-auto py-28">
-        <div className="w-1/2">
-        <Image src={Star} width={30} height={36} alt="" className="w-4 md:w-8" />
-          <Image src={Judges} width={710} height={587} alt="" className="" />
-          <Image src={Star1} width={21} height={25} alt="" className="relative z-50" />
+      <div className="md:w-10/12 px-5 flex flex-col md:flex-row mx-auto py-20">
+        <div className="md:w-1/2 flex flex-col justify-center items-center">
+        <Image src={Star} width={30} height={36} alt="" className="w-4 md:w-8 mr-auto ml-16 relative -mt-10" />
+          <di className="py-5">
+          <Image src={Ellipse} width={134} height={134} alt="" className="hidden md:block" />
+          <Image src={Judges} width={710} height={587} alt="" className="relative z-20 md:-mt-20 md:-ml-20" />
+          </di>
+          <Image src={Star1} width={21} height={25} alt="" className="ml-auto mr-20" />
         </div>
-        <div className="w-1/2">
-          <h2>
+        <div className="md:w-1/2 text-center">
+          <h2 className="text-2xl md:text-3xl font-bold tracking-tight leading-tight py-8">
             Judging Criteria <br />
-            <span>Key attributes</span>
+            <span className="text-[#d434fe]">Key attributes</span>
           </h2>
-          <p>
-            Innovation and Creativity: Evaluate the uniqueness and creativity of
+        <div className="flex flex-col gap-y-6 text-lg md:text-md">
+        <p>
+            <span className="text-[#ff26b9] font-semibold">Innovation and Creativity</span>: Evaluate the uniqueness and creativity of
             the solution. Consider whether it addresses a real-world problem in
             a novel way or introduces innovative features.
           </p>
           <p>
-            Functionality: Assess how well the solution works. Does it perform
+            <span className="text-[#ff26b9] font-semibold">Functionality:</span> Assess how well the solution works. Does it perform
             its intended functions effectively and without major issues? Judges
             would consider the completeness and robustness of the solution.
           </p>
           <p>
-            Impact and Relevance: Determine the potential impact of the solution
+           <span className="text-[#ff26b9] font-semibold">Impact and Relevance:</span> Determine the potential impact of the solution
             in the real world. Does it address a significant problem, and is it
             relevant to the target audience? Judges would assess the potential
             social, economic, or environmental benefits.
           </p>
           <p>
-            Technical Complexity: Evaluate the technical sophistication of the
+            <span className="text-[#ff26b9] font-semibold">Technical Complexity:</span> Evaluate the technical sophistication of the
             solution. Judges would consider the complexity of the code, the use
             of advanced technologies or algorithms, and the scalability of the
             solution.
           </p>
           <p>
-            Adherence to Hackathon Rules: Judges will Ensure that the team
+            <span className="text-[#ff26b9] font-semibold">Adherence to Hackathon Rules:</span> Judges will Ensure that the team
             adhered to the rules and guidelines of the hackathon, including
             deadlines, use of specific technologies or APIs, and any other
             competition-specific requirements.
           </p>
-          <button className="button px-7 py-2 rounded-sm my-3 mx-auto md:mx-0">
+        </div>
+          <button className="button px-7 py-2 rounded-sm my-8 mx-auto md:mx-0">
             Read More
           </button>
         </div>
--- app/Rules.tsx
+++ app/Rules.tsx
@@ -8,8 +8,8 @@ import PurpleLens from "../public/images/Purple-Lens.svg";
 const Rules = () => {
   return (
     <div className="bg-[#150E28] text-white border-b border-[#3d364d]">
-      <div className="flex flex-row w-10/12 mx-auto gap-x-20 py-16">
-        <div className="w-1/2 flex flex-col py-6">
+      <div className="flex flex-col-reverse px-5 py-10 md:flex-row md:w-10/12 mx-auto gap-x-20 md:py-16">
+        <div className="md:w-1/2 flex flex-col text-center md:text-left py-6">
           <div className="flex flex-row justify-center items-center ">
           <Image src={Star1} width={21} height={25} alt="" className="relative z-50" />
           <Image
@@ -17,10 +17,10 @@ const Rules = () => {
             width={600}
             height={150}
             alt=""
-            className="absolute w-10/12 brightness-50 mt-[60px]"
+            className="absolute w-full brightness-75 md:w-10/12 md:brightness-50 mt-[70px]"
           />
           </div>
-          <h2 className="z-10 text-3xl font-bold tracking-tight leading-tight py-8">
+          <h2 className="z-10 text-2xl md:text-3xl font-bold tracking-tight leading-tight py-8">
             Rules and <br />
             <span className="text-[#d434fe]">Guidelines</span>
           </h2>
@@ -34,11 +34,11 @@ const Rules = () => {
           </p>
           <Image src={Star} width={21} height={25} alt="" className="ml-auto" />
         </div>
-        <div className="w-1/2 flex flex-col items-center">
-          <div className="absolute -mt-8 ml-32">
+        <div className="md:w-1/2 flex flex-col items-center">
+          <div className="hidden md:absolute md:-mt-8 md:ml-32">
           <Image src={Ellipse} width={134} height={134} alt="" className="" />
           </div>
-         <div className="absolute">
+         <div className="md:absolute">
          <Image src={Lady} width={664} height={664} alt="" className="" />
          </div>
         </div>
