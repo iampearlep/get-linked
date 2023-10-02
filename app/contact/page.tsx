bd5e10a826ed397be5f63a491c3128602e15ffdf
iampearlep <damilolaprincess4@gmail.com>
2023-10-02 16:01:55 +0100

    update components

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,27 @@
-export default function Page() {
-    return <h1>Hello contact, Next.js!</h1>
-  }
+export default function About() {
+  return (
+    <div className="bg-[#150E28] text-white">
+      <div>
+        <div>
+          <h2>Get in touch</h2>
+          <p>
+            Contact <br />
+            Information
+          </p>
+          <p>
+            27,Alara Street <br />
+            Yaba 100012 <br />
+            Lagos State
+          </p>
+          <p>Call Us : 07067981819</p>
+          <p>we are open from Monday-Friday 08:00am - 05:00pm</p>
+          <p>Share on</p>
+        </div>
+        <div>
+          <h2>Questions or need assistance? Let us know about it</h2>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
